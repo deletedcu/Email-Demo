22d84c5f8f9be32d00bb651439fcd363c96edc24
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2019-02-27 10:26:26 +0800

    updated index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(3000);
+app.listen(9000);
