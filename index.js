4f647dea121e6f828012d7d821a223287b8d2a3f
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2019-02-27 10:54:36 +0800

    change port

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.set('port', (process.env.PORT || 9000))
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+app.listen(app.get('port'), function () {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
