b73c6591178fb59870f71515e0e5c40c6289b2b1
CUREST TREBOW <43941751+curest0x1021@users.noreply.github.com>
2019-02-27 10:17:52 +0800

    add build folder

---
+++ build/precache-manifest.e9e59eea6d6432b48e67beee3012e6e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ffda023c80022735e162",
+    "url": "/static/js/main.ffda023c.chunk.js"
+  },
+  {
+    "revision": "f166c89ea5d7894f2a52",
+    "url": "/static/js/2.f166c89e.chunk.js"
+  },
+  {
+    "revision": "ffda023c80022735e162",
+    "url": "/static/css/main.50385e4a.chunk.css"
+  },
+  {
+    "revision": "f166c89ea5d7894f2a52",
+    "url": "/static/css/2.702012e6.chunk.css"
+  },
+  {
+    "revision": "63fdbb78808e05229ab4382b0399b1b1",
+    "url": "/index.html"
+  }
+];
